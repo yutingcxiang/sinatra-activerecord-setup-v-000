22db4e41bdd2d2ed0d318b55adf94291b6ed1e3b
Christine Xiang <yutingcxiang@gmail.com>
2018-08-20 23:10:46 +0000

    automatically backed up by learn

--- db/migrate/20180820230750_create_dogs.rb
+++ db/migrate/20180820230750_create_dogs.rb
@@ -1,12 +1,8 @@
 class CreateDogs < ActiveRecord::Migration
-  def up
+  def change
     create_table :dogs do |t|
       t.string :name
       t.string :breed
     end
-  end
-  
-  def down
-    drop_table :dogs
   end
 end
